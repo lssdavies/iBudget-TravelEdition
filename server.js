6cfd2eb45b28952e7b2cf7f722928a432ba97daf
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-03-24 15:32:02 -0700

    db connection

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const compression = require("compression");
 
 //due to an issue with my mongoDB I have to ip address /127.0.0.1 instead of local host
 const PORT = process.env.PORT || 3001;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/pizza-hunt", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const MONGODB_URI = mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1/pizza-hunt",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const app = express();
 
