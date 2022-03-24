46d4a44967c13b9da0bb44646eb8fb35dbc7bf5c
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-03-24 16:17:27 -0700

    address issues with manifest.json and service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,8 +6,17 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/css/styles.css",
+  "/manifest.json",
   "/js/index.js",
   "/js/idb.js",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png",
 ];
 
 // Respond with cached resources
